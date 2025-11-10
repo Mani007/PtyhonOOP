c4f91d4e9a4a29dfe9c8f066c31bce10d8569851
Mohnish <mohnish.a007@gmail.com>
2025-11-10 15:18:29 +0000

    method to instance of class

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 class item:
-    pass
+    def calculate(self, x , y):
+        print("The total price is " + str(x*y))
 
 item1 = item() #instance of class item
 item1.name = "abc" # adding attribute to the instance of class
-item1.age = 10
-print("Item1 name is " + item1.name + " item1 age is " + str(item1.age) )
+item1.price = 10
+item1.qty = 5
+print("Item1 name is " + item1.name + " item1 price is " + str(item1.price) + " item1 qty is " + str(item1.qty))
+item1.calculate(item1.price, item1.qty)
+## new instance
+item2 = item() #instance of class item
+item2.name = "xyz" # adding attribute to the instance of class
+item2.price = 100
+item2.qty = 20
+print("Item1 name is " + item2.name + " item1 price is " + str(item2.price) + " item2 qty is " + str(item2.qty))
+item2.calculate(item2.price, item2.qty)
