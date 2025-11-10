409ddf06fe3db956d149f401fb5c236756586525
Mohnish <mohnish.a007@gmail.com>
2025-11-10 12:43:11 +0000

    attribute to instance of class

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ class item:
     pass
 
 item1 = item() #instance of class item
-item1.name = "abc"
+item1.name = "abc" # adding attribute to the instance of class
 item1.age = 10
 print("Item1 name is " + item1.name + " item1 age is " + str(item1.age) )
