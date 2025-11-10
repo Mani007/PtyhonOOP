9db57360850ee3168528685ac8d3e2a7ad0dd709
Mohnish <mohnish.a007@gmail.com>
2025-11-10 16:45:51 +0000

    calculate from the object

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ class item:
         self.name = name 
         self.price = price
         self.qty = qty
-    def calculate(self, x , y):
-        print("The total price is " + str(x*y))
+    def calculate(self):
+        print("The total price is " + str(self.price*self.qty))
 
 item1 = item('abc',10,5) #instance of class item
 # item1.name = "abc" # adding attribute to the instance of class
 # item1.price = 10
 # item1.qty = 5
 print("Item1 name is " + item1.name + " item1 price is " + str(item1.price) + " item1 qty is " + str(item1.qty))
-item1.calculate(item1.price, item1.qty)
+item1.calculate()
 ## new instance
 item2 = item('xyz',100,25) #instance of class item
 # item2.name = "xyz" # adding attribute to the instance of class
 # item2.price = 100
 # item2.qty = 20
 print("Item1 name is " + item2.name + " item1 price is " + str(item2.price) + " item2 qty is " + str(item2.qty))
-item2.calculate(item2.price, item2.qty)
+item2.calculate()
